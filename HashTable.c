1e1890e42ca46aed956aed9e4ab5f049c46bbddd
Stephen <sheldon2521@gmail.com>
2019-03-15 11:27:39 -0400

    Added remove and print table functions

--- HashTable.c
+++ HashTable.c
@@ -10,6 +10,12 @@
 ** Insert: O(n) - Worst case. Ex: We have a key with a long chain and due to
 **				  chains be lexicographically sorted we somehow end up having
 **                to insert the element at the end of the chain.
+** Delete: O(1) - Assuming we don't have huge chains to search
+** Delete: O(n) - Worst case, we have to search a chain at the hash index.
+**
+** Notes: At the moment this hash table is going to hit worst case scenarios the majority of the time
+**        due to the size of the hash table along with the number of inputs. The main purpose of
+**        this hash table is to demonstrate chaining versus optimizing the time complexity. 
 **
 ** Hash Table stores people
 ** People have first and last names, sometimes the same.
@@ -27,6 +33,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // Max length of a person's first or last name.
 #define MAX_LEN 100
@@ -54,15 +61,14 @@ struct hashtable* new_hashtable();
 void insert(struct hashtable* h, struct person* p);
 int hash_function(char first_name[], char last_name[]); 
 void delete_hashtable(struct hashtable* h); 
-void lookup(struct hashtable* h, char first_name[], char last_name[]);
+int lookup(struct hashtable* h, char first_name[], char last_name[]);
+int remove_person(struct hashtable* h, char first_name[], char last_name[], int pid);
+void print_table(struct hashtable* h);
 char * to_lowercase(char s[]);
-
-//void expand(struct hashtable* h);
-//int remove(struct hashtable* h, struct person* p);
 
 int main(void)
 {
-	int i, choice, num_entries;
+	int i, choice, num_entries, pid, seen;
 	FILE *fp;
 	char* token;
 	char first_name[MAX_LEN];
@@ -101,11 +107,15 @@ int main(void)
 	printf("Maybe you want us to look and see if we have someone you know?\n\n\n");
 	
 	do {
+		seen = 0;
+		
 		printf("1. Look up by first and last name\n");
+		printf("2. Remove a person by first and last name\n");
+		printf("3. Print hash table\n");
 		printf("0. Exit program\n");
 		scanf("%d", &choice);
 		
-		if (choice == 1)
+		if (choice == 1 || choice == 2)
 		{
 			printf("Please enter the first and last name, separated by a space\n");
 			scanf("%s %s", first_name, last_name);
@@ -114,8 +124,20 @@ int main(void)
 			strcpy(first_name, to_lowercase(first_name));
 			strcpy(last_name, to_lowercase(last_name));
 			
-			lookup(my_hashtable, first_name, last_name);
+			seen = lookup(my_hashtable, first_name, last_name);
 		}
+		if (choice == 2 && seen != 0)
+		{
+			printf("Now that we found the person you wish to remove, please enter their ID to delete them\n");
+			scanf("%d", &pid);
+			
+			if (remove_person(my_hashtable, first_name, last_name, pid))
+				printf("Successfully removed %s %s ID: %d\n", first_name, last_name, pid);
+			else
+				printf("There was an error removing %s %s, please make sure you entered the correct ID\n", first_name, last_name);
+		}
+		else if (choice == 3)
+			print_table(my_hashtable);
 		
 	} while (choice != 0);
 	
@@ -201,7 +223,7 @@ int hash_function(char first_name[], char last_name[])
 }
 
 // Prints all people in the database matching the first and last name
-void lookup(struct hashtable* h, char first_name[], char last_name[])
+int lookup(struct hashtable* h, char first_name[], char last_name[])
 {
 	int hash_index;
 	int seen;
@@ -214,24 +236,90 @@ void lookup(struct hashtable* h, char first_name[], char last_name[])
 
 	current_node = h->store[hash_index];
 	seen = 0;
-	
+	printf("\n");
 	while (current_node != NULL) 
 	{	
 		if (strcmp(strcpy(temp_first, to_lowercase(current_node->first_name)), first_name) == 0 &&
 			strcmp(strcpy(temp_last, to_lowercase(current_node->last_name)), last_name) == 0)
 		{
 			seen = 1;
-			printf("\nFound name: %s %s  ", current_node->first_name, current_node->last_name);
+			printf("Found name: %s %s  ", current_node->first_name, current_node->last_name);
 			printf("Personal ID: %d\n", current_node->id);
 		}
 		current_node = current_node->next;
 	}	
 	
 	if (seen == 0)
-		printf("\nI'm sorry, I could not find %s %s in the database\n", first_name, last_name);
-		
+	{
+		printf("I'm sorry, I could not find %s %s in the database\n\n", first_name, last_name);
+		return 0;
+	}
 	printf("\n");
+	return 1;
 }
+
+int remove_person(struct hashtable* h, char first_name[], char last_name[], int pid)
+{
+	// if successfully removed return 1
+	int hash_index;
+	struct person* current_node;
+	struct person* temp;
+	char temp_first[MAX_LEN];
+	char temp_last[MAX_LEN];
+	
+	// Grab hash index
+	hash_index = hash_function(first_name, last_name);
+
+	// Case: Head is null 
+	if (h->store[hash_index] == NULL)
+		return 0;
+	
+	// Case: Head of chain is the person we are looking for, normally we could just check
+	//       if the ID is the one we're looking for, but for the sake of debugging to make
+	//       sure our input file doesn't have duplicate ID's, let's check the first and last name.
+	if (strcmp(strcpy(temp_first, to_lowercase(h->store[hash_index]->first_name)), first_name) == 0 &&
+		strcmp(strcpy(temp_last, to_lowercase(h->store[hash_index]->last_name)), last_name) == 0 &&
+		pid == h->store[hash_index]->id)
+	{
+		temp = h->store[hash_index];
+		if (h->store[hash_index]->next == NULL)
+		
+			h->store[hash_index] = NULL;
+		else 
+		{ 	
+			h->store[hash_index] = h->store[hash_index]->next;
+		}
+		
+		free(temp);
+			
+		return 1;
+	}
+	
+	// Set current node to point at beginning of potential chain at hash index
+	current_node = h->store[hash_index];
+	
+	// Because the node is sorted lexicographically by the first name, we can stop if
+	// the first name of our current node comes before (lexicographically) than the name we're
+	// looking for.
+	while (current_node->next != NULL &&
+		   strcmp(strcpy(temp_first, to_lowercase(current_node->next->first_name)), first_name) < 0)
+	{
+		current_node = current_node->next;
+	}
+	
+	// Case: Either the next node is NULL OR the current node COULD be the person
+	//       we are looking for. At this point we really only need to check the id 
+	//       because that's what we're basing our deletes off of.
+	if (current_node->next == NULL || current_node->next->id != pid)
+		return 0;
+		
+	// Case: We've found the matching person!
+	temp = current_node->next;
+	current_node->next = current_node->next->next;
+	free(temp);
+	return 1;	
+}
+
 
 // Free all memory allocated to hash table and it's elements
 void delete_hashtable(struct hashtable* h) 
@@ -258,6 +346,33 @@ void delete_hashtable(struct hashtable* h)
 	free(h);
  } 
  
+ void print_table(struct hashtable* h)
+ {
+	int i;
+	struct person* current_node;
+	
+	for (i = 0; i < h->length; i++)
+	{
+		printf("Hash index %d ", i);
+		current_node = h->store[i];	
+		
+		// Case: Head at index is null, continue to next hash index
+		if (current_node == NULL)
+		{
+			printf("there is no one here!\n\n");
+			continue;
+		}
+		
+		printf("we have:\n");
+		while (current_node != NULL) 
+		{
+			printf("%s %s ID: %d\n", current_node->first_name, current_node->last_name, current_node->id);
+			current_node = current_node->next;
+		}
+		printf("\n");
+	}
+ }
+
  // Convert a string to all lowercase
  char * to_lowercase(char s[])
  {
@@ -268,5 +383,5 @@ void delete_hashtable(struct hashtable* h)
 	
 	return s; 
  }
-//void expand(struct hashtable* h); 
-//int remove(struct hashtable* h, struct person* p);
+
+
